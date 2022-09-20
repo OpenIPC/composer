4332c7b9e95ae7864c026e9d693ad4e8b6d515f0
Igor Zalatov (from Citadel PC) <flyrouter@gmail.com>
2022-09-20 16:34:24 +0300

    Sync of comgt work

--- packages/comgt/comgt.mk
+++ packages/comgt/comgt.mk
@@ -8,16 +8,16 @@ COMGT_LICENSE = GPL
 COMGT_LICENSE_FILES = LICENSE
 
 define COMGT_EXTRACT_CMDS
-	cp ../general/package/demo-openipc/src/* $(@D)/
+	cp -a ../general/package/comgt/* $(@D)/
 endef
 
 define COMGT_BUILD_CMDS
-	(cd $(@D); $(TARGET_CC) -s comgt.c -o comgt)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/src
 endef
 
 define COMGT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
-	install -m 0755 -D $(@D)/comgt $(TARGET_DIR)/usr/sbin/comgt
+	install -m 0755 -D $(@D)/src/comgt $(TARGET_DIR)/usr/sbin/comgt
 endef
 
 $(eval $(generic-package))
