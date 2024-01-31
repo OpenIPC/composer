aef05b8436c738a35c30697c326ff90cf6a228e7
Signor Pellegrino (from Citadel PC) <flyrouter@gmail.com>
2024-01-31 10:08:18 +0300

    Update bt656 projects

--- projects/gk7205v200_lite_huahui/general/overlay/usr/bin/bt656_ahd_pal_720p.sh
+++ projects/gk7205v200_lite_huahui/general/overlay/usr/bin/bt656_ahd_pal_720p.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+(devmem 0x112c0048 32 0x0 ; sleep 0.1 ; gpio clear 71 ; sleep 0.1 ; gpio set 71 ; sleep 0.1) >/dev/null
+
 ipctool2 i2cset 0x88 0x02 0xCE
 ipctool2 i2cset 0x88 0x05 0x00
 ipctool2 i2cset 0x88 0x06 0x32
